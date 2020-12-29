2d109e33b0cd919066cc3931e676f080c27534c3
Carlos Zamorano <carlos.zamorano@gus.global>
2020-12-29 01:48:46 +0100

    delete inner proyect

--- Viaja_al_pasado_CarlosZamorano/src/app/forbidden-inputs/forbidden-inputs.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ForbiddenInputsDirective } from './forbidden-inputs.directive';
-
-describe('ForbiddenInputsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ForbiddenInputsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Viaja_al_pasado_CarlosZamorano/src/app/pipes/ordenar-precio.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OrdenarPrecioPipe } from './ordenar-precio.pipe';
-
-describe('OrdenarPrecioPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OrdenarPrecioPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
